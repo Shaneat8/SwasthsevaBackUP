4a3fb61875b90ea9eaf94ea11784a27789e80d5b
Shaneat8 <ambujtripathi41@gmail.com>
2025-03-01 11:38:08 +0530

    netlify

--- src/pages/DoctorForm/PatientRecordsPage.js
+++ src/pages/DoctorForm/PatientRecordsPage.js
@@ -24,6 +24,7 @@ import {
 import moment from "moment";
 
 const { Title, Text } = Typography;
+//trying to deploy on netlify
 
 function PatientRecordsPage() {
   const [patientData, setPatientData] = useState(null);
